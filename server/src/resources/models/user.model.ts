c878775359094678ca2f10b31e170bced3d6f71b
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-07-05 14:40:35 +0100

    user model

---
+++ server/src/resources/models/user.model.ts
@@ -0,0 +1,17 @@
+import { Schema, model } from 'mongoose';
+import IUser from '../interfaces/user.interface';
+
+const userSchema = new Schema<IUser>({
+  name: { type: String, required: [true, 'You must have name'] },
+  reg_no: { type: String },
+  department: { type: String },
+  gender: { type: String },
+  password: { type: String, required: [true, 'password is required'] },
+  role: {
+    type: String,
+    enum: ['admin', 'student'],
+    default: 'student',
+  },
+});
+
+export default model('User', userSchema);
