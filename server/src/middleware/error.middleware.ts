3ebd205626932afd26101091036d2ad3409c2f7e
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-07-05 14:44:08 +0100

    linting

--- server/src/middleware/error.middleware.ts
+++ server/src/middleware/error.middleware.ts
@@ -1,6 +1,7 @@
 import {Request, Response, NextFunction} from 'express'
 import HttpException from '@/utils/exceptions/httpExceptions'
 
+
 function errorMiddleware (err: HttpException, req: Request, res: Response, next: NextFunction):void {
     const statusCode = err.statusCode || 500
     const message = err.message || 'Something went wrong'
